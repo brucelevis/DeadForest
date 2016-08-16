438c149466adda3f202d92d26188e43f093ad7d8
NamJunHyone <njh0602@naver.com>
2016-08-17 03:25:12 +0900

    goal 폴더 구분, GoalKillPlayer 파일 생성

---
+++ DeadCreator/Classes/client/ai/goal/composite_goals/GoalKillPlayer.cpp
@@ -0,0 +1,9 @@
+//
+//  GoalKillPlayer.cpp
+//  DeadCreator
+//
+//  Created by mac on 2016. 8. 17..
+//
+//
+
+#include "GoalKillPlayer.hpp"
---
+++ DeadCreator/Classes/client/ai/goal/composite_goals/GoalKillPlayer.hpp
@@ -0,0 +1,14 @@
+//
+//  GoalKillPlayer.hpp
+//  DeadCreator
+//
+//  Created by mac on 2016. 8. 17..
+//
+//
+
+#ifndef GoalKillPlayer_hpp
+#define GoalKillPlayer_hpp
+
+#include <stdio.h>
+
+#endif /* GoalKillPlayer_hpp */

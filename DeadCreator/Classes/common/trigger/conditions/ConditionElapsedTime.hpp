d4c1c4cdd14b24a74f67a3f2ff4feb235b65b546
NamJunHyone <njh0602@naver.com>
2016-10-17 02:49:41 +0900

    add action, condition client implement

--- DeadCreator/Classes/common/trigger/conditions/ConditionElapsedTime.hpp
+++ DeadCreator/Classes/common/trigger/conditions/ConditionElapsedTime.hpp
@@ -110,98 +110,56 @@ namespace realtrick
     namespace client
     {
         
-//        struct ConditionElapsedTimeData: public TriggerDataBase
-//        {
-//            ApproximationType approximation;
-//            int number;
-//            
-//            ConditionElapsedTimeData() { type = TriggerComponentType::CONDITION_ELAPSED_TIME; }
-//        };
-//        
-//        class ConditionElapsedTime : public ConditionBase
-//        {
-//            
-//        public:
-//            
-//            explicit ConditionElapsedTime(Game* game) : ConditionBase(game)
-//            {
-//            }
-//            
-//            virtual ~ConditionElapsedTime() = default;
-//            
-//            static ConditionElapsedTime* create(Game* game, ApproximationType appType, int number)
-//            {
-//                auto ret = new (std::nothrow) ConditionElapsedTime(game);
-//                if ( ret && ret->init(appType, number) )
-//                {
-//                    ret->autorelease();
-//                    return ret;
-//                }
-//                CC_SAFE_DELETE(ret);
-//                return nullptr;
-//            }
-//            
-//            bool init(ApproximationType appType, int number)
-//            {
-//                _params.approximation = appType;
-//                _params.number = number;
-//                
-//                return true;
-//            }
-//            
-//            virtual bool isReady() override
-//            {
-//                if ( _params.player == PlayerType::CURRENT_PLAYER ) _maskedPlayer = _owner->getPlayers();
-//                    else _maskedPlayer.set(static_cast<int>(_params.player));
-//                        
-//                        const auto& entities = _game->getEntityManager()->getEntities();
-//                        const auto& locations = _game->getGameResource()->getLocations();
-//                        
-//                        // 플레이어 타입인지 체크. (o)
-//                        // 엔티티 타입 체크. (o)
-//                        // 충돌됐는지 체크. (o)
-//                        // 숫자맞는지 체크. (x)
-//                        int numberOfReadyEntities = 0;
-//                for( auto& ent : entities)
-//                {
-//                    auto entity = ent.second;
-//                    int player = static_cast<int>(entity->getPlayerType());
-//                    
-//                    if ( _maskedPlayer.test(player) && _params.entity == entity->getEntityType())
-//                    {
-//                        bool intersect = false;
-//                        if ( isMasked(ent.second->getFamilyMask(), FamilyMask::HUMAN_BASE) )
-//                        {
-//                            HumanBase* human = static_cast<HumanBase*>(ent.second);
-//                            intersect = (human->isAlive() &&
-//                                         locations.at(_params.location).intersectsCircle(entity->getWorldPosition(), entity->getBoundingRadius()));
-//                            
-//                        }
-//                        else
-//                        {
-//                            intersect = locations.at(_params.location).intersectsCircle(entity->getWorldPosition(), entity->getBoundingRadius());
-//                        }
-//                        
-//                        if ( intersect )
-//                        {
-//                            numberOfReadyEntities++;
-//                        }
-//                    }
-//                }
-//                
-//                if ( _params.approximation == ApproximationType::AT_LEAST && numberOfReadyEntities >= _params.number ) return true;
-//                else if ( _params.approximation == ApproximationType::AT_MOST && numberOfReadyEntities <= _params.number ) return true;
-//                else if ( _params.approximation == ApproximationType::EXACTLY && numberOfReadyEntities == _params.number ) return true;
-//                
-//                return false;
-//            }
-//
-//        private:
-//            
-//            ConditionElapsedTimeData _params;
-//            std::bitset<30> _maskedPlayer;
-//            
-//        };
+        struct ConditionElapsedTimeData: public TriggerDataBase
+        {
+            ApproximationType approximation;
+            int number;
+            
+            ConditionElapsedTimeData() { type = TriggerComponentType::CONDITION_ELAPSED_TIME; }
+        };
+        
+        class ConditionElapsedTime : public ConditionBase
+        {
+            
+        public:
+            
+            explicit ConditionElapsedTime(Game* game) : ConditionBase(game)
+            {
+            }
+            
+            virtual ~ConditionElapsedTime() = default;
+            
+            static ConditionElapsedTime* create(Game* game, ApproximationType appType, int number)
+            {
+                auto ret = new (std::nothrow) ConditionElapsedTime(game);
+                if ( ret && ret->init(appType, number) )
+                {
+                    ret->autorelease();
+                    return ret;
+                }
+                CC_SAFE_DELETE(ret);
+                return nullptr;
+            }
+            
+            bool init(ApproximationType appType, int number)
+            {
+                _params.approximation = appType;
+                _params.number = number;
+                
+                return true;
+            }
+            
+            virtual bool isReady() override
+            {
+                return false;
+            }
+            
+        private:
+            
+            ConditionElapsedTimeData _params;
+            std::bitset<30> _maskedPlayer;
+            
+        };
         
     }
     
